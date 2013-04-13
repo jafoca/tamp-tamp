ff5b79d28cf306183ad48f7d1d80b6cf7b28eda6
jafoca <nicholas.defoe@gmail.com>
2013-04-13 15:24:43 -0400

    update asset pipeline settings for Heroku:

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,10 @@ module TampTamp
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # config/application.rb
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
